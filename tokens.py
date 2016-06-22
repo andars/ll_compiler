39995d31726e13b0e1b9d18055cdae3716d309c0
andars <afoote97@gmail.com>
2016-06-22 15:22:09 -0700

    expose Token's data externally

--- tokens.py
+++ tokens.py
@@ -7,14 +7,19 @@ class Token(Enum):
     UNKNOWN = 5
     EOF = 6
 
-
     def __str__(self):
         s = "<" + str(self.name)
-        if hasattr(self, 'data'):
-            return s + ": " + str(self.data) + ">"
+        if hasattr(self, '_data'):
+            return s + ": " + str(self._data) + ">"
         return s + ">"
     
 
     def __call__(self, data):
-        self.data = data
+        self._data = data
         return self
+
+    @property
+    def data(self):
+        if hasattr(self, '_data'):
+            return self._data
+        return None
