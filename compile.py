27d609824eaa82fa019d878de20a56799cea70d5
Andrew Foote <afoote97@gmail.com>
2016-06-22 20:40:14 -0700

    split compiler's `test.txt` from `parsetest.txt`

--- compile.py
+++ compile.py
@@ -2,7 +2,7 @@ from lexer import Lexer
 from parser import Parser
 from compiler import Compiler
 
-with open('parsetest.txt') as f:
+with open('test.txt') as f:
     lexer = Lexer(f)
     parser = Parser(lexer)
     parsed = parser.parse()
